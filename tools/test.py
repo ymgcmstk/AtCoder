2a7872a47578c4ed606fb48b39ab5baa4536af26
ymgcmstk <yamaguchi@mi.t.u-tokyo.ac.jp>
2015-12-02 02:26:49 +0900

    add a function to copy to clipboard

--- tools/test.py
+++ tools/test.py
@@ -28,6 +28,7 @@ def main():
 
     inputs, outputs = split_io(targ_dir)
     assert len(inputs) == len(outputs)
+    pass_all = True
     for i, cur_i, cur_o in zip(range(len(inputs)), inputs, outputs):
         p = subprocess.Popen(['./%s' % targ_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         p.stdin.write(cur_i)
@@ -37,8 +38,16 @@ def main():
             print stdout_data.rstrip('\n')
             print 'output:'
             print cur_o.rstrip('\n')
+            pass_all = False
         else:
             print 'Passed TEST %d' % (i + 1)
+    if os.uname()[0] == 'Darwin' and pass_all:
+        source_name = targ_file
+        if not targ_file.endswith('.py'):
+            source_name += '.cpp'
+        p = subprocess.Popen(['cat %s | pbcopy' % os.path.join(os.getcwd(), source_name)], shell=True)
+        p.wait()
+        print 'Copy %s to clipboard' % source_name
     return
 
 if __name__ == '__main__':
