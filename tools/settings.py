06525bf7ac31495c0b748e40e72493383996b469
ymgcmstk <yamaguchi@mi.t.u-tokyo.ac.jp>
2015-12-22 16:39:42 +0900

    auto-submission

--- tools/settings.py
+++ tools/settings.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 import os
 # Settings
-ROOT             = '/path/to/your/rep'
+ROOT             = '/Users/yamaguchi/myproject/AtCoder/'
 TOOL_DIR         = os.path.join(ROOT, 'tools')
 ABC_DIR          = os.path.join(ROOT, 'ABC')
 ARC_DIR          = os.path.join(ROOT, 'ARC')
@@ -11,5 +11,12 @@ TEMPLATE         = os.path.join(TOOL_DIR, 'template.cpp')
 FNAME            = ['A', 'B', 'C', 'D']
 INPUT_FILE_NAME  = 'input.txt'
 OUTPUT_FILE_NAME = 'output.txt'
-
 DELIMITER = "---"
+BASE_URL = 'http://%s.contest.atcoder.jp/'
+
+# Auto-submission
+USER_NAME = 'XXX'
+PASSWORD  = 'YYY'
+LANG_CPP  = 'C++11 (GCC 4.8.1)'
+LANG_PY   = 'Python (2.7.3)'
+BROWSER   = 'chrome'
--- tools/test.py
+++ tools/test.py
@@ -5,6 +5,9 @@ import sys
 import subprocess
 import os
 from settings import *
+import splinter
+import Levenshtein
+import time
 
 def split_io(targ_dir):
     f = open(os.path.join(targ_dir, INPUT_FILE_NAME))
@@ -42,13 +45,61 @@ def main():
         else:
             print 'Passed TEST %d' % (i + 1)
     if os.uname()[0] == 'Darwin' and pass_all:
+        submit()
+        """
         source_name = targ_file
         if not targ_file.endswith('.py'):
             source_name += '.cpp'
         p = subprocess.Popen(['cat %s | pbcopy' % os.path.join(os.getcwd(), source_name)], shell=True)
         p.wait()
         print 'Copy %s to clipboard' % source_name
+        """
     return
+
+def submit():
+    contest_name, sub_name = os.getcwd().split('/')[-2:]
+    if os.path.exists(os.path.join(sub_name)) or True:
+        f_name = sub_name + '.cpp'
+    else:
+        assert os.path.exists('%s.py' % sub_name)
+        f_name = sub_name + '.py'
+    browser = splinter.Browser(BROWSER)
+    # sign in
+    signin_url = os.path.join(BASE_URL % contest_name, 'login')
+    browser.visit(signin_url)
+    browser.fill('name', USER_NAME)
+    browser.fill('password', PASSWORD)
+    browser.find_by_xpath('//*[@id="outer-inner"]/div[2]/div[1]/form/fieldset/div[3]/button').click()
+
+    # submit
+    submit_url = os.path.join(BASE_URL % contest_name, 'submit')
+    browser.find_by_xpath('//*[@id="outer-inner"]/ul/li[3]/a/span/span[2]').click()
+    f = open(f_name, 'r')
+    browser.fill('source_code', ''.join(f.read()))
+    f.close()
+    elements = browser.find_by_xpath('//*[@id="submit-task-selector"]/option')
+    for i in elements:
+        if i.text.startswith(sub_name):
+            i.click()
+            break
+    elements = browser.find_by_xpath('//*[@id="submit-language-selector-1009"]/option')
+    dist = float('inf')
+    for i in elements:
+        if f_name.startswith('py'):
+            i_dist = Levenshtein.distance(i.text, unicode(LANG_PY))
+        else:
+            i_dist = Levenshtein.distance(i.text, unicode(LANG_CPP))
+        if i_dist < dist:
+            dist = i_dist
+            i.click()
+    browser.find_by_xpath('//*[@id="outer-inner"]/div[2]/div/form/fieldset/div[4]/button/span/span[2]').click()
+    cur_status = browser.find_by_xpath('//*[@id="outer-inner"]/table/tbody/tr[1]/td[5]/span').text
+    while cur_status == 'WJ' or '/' in cur_status:
+        printr(cur_status)
+        time.sleep(5)
+        browser.reload()
+        cur_status = browser.find_by_xpath('//*[@id="outer-inner"]/table/tbody/tr[1]/td[5]/span').text
+    print cur_status + ' ' * 20
 
 if __name__ == '__main__':
     main()
